56fdb4115675bb4d50bef8695d97c3f8c85b8fd6
ben10 <ben10@debian.https://github.com/pk8742/>
2020-10-08 13:27:07 +0530

    New v1.0

--- application.py
+++
@@ -1,7 +0,0 @@
-
-import tkinter as tk
-r = tk.Tk() # creating main window object, where r is the name of the main window object
-r.title('Counting Seconds')
-button = tk.Button(r, text='Stop', width=25, command=r.destroy)
-button.pack() # pack() organizes the widgets in blocks before placing in the parent widget.
-r.mainloop() # mainloop() is an infinite loop used to run the application, wait for an event to occur.
